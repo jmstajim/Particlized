f230d67dc855abd63d289398a3ad2fdd09f319f0
Aleksey Gusachenko <gusachenkoalexius@gmail.com>
2025-09-17 00:30:35 +0300

    ParticlizedRenderer alpha blend

--- Sources/Particlized/Rendering/Renderer/ParticlizedRenderer.swift
+++ Sources/Particlized/Rendering/Renderer/ParticlizedRenderer.swift
@@ -110,6 +110,13 @@ public final class ParticlizedRenderer: NSObject, MTKViewDelegate {
         rpd.vertexFunction = vs
         rpd.fragmentFunction = fs
         rpd.colorAttachments[0].pixelFormat = .bgra8Unorm
+        rpd.colorAttachments[0].isBlendingEnabled = true
+        rpd.colorAttachments[0].rgbBlendOperation = .add
+        rpd.colorAttachments[0].alphaBlendOperation = .add
+        rpd.colorAttachments[0].sourceRGBBlendFactor = .sourceAlpha
+        rpd.colorAttachments[0].destinationRGBBlendFactor = .oneMinusSourceAlpha
+        rpd.colorAttachments[0].sourceAlphaBlendFactor = .one
+        rpd.colorAttachments[0].destinationAlphaBlendFactor = .oneMinusSourceAlpha
         
         do {
             renderPipeline = try device.makeRenderPipelineState(descriptor: rpd)
@@ -284,4 +291,3 @@ public final class ParticlizedRenderer: NSObject, MTKViewDelegate {
         frameIndex = (frameIndex + 1) % inflightBuffers
     }
 }
-
